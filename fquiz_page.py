aabbc4e74a0f3b04c5dbaa66281b3e5261efec12
ggcio <168268845+ggcio@users.noreply.github.com>
2024-12-23 14:28:50 +0800

    Update fquiz_page.py

--- fquiz_page.py
+++ fquiz_page.py
@@ -34,7 +34,7 @@ class flashcardQuizzer:
         tk.Button(btn_frame, text="Study Quiz", font=arial_round, bg='#d19711', activebackground='#0ee832', relief="sunken", bd=5, padx=7, pady=3, command=self.study_quiz).pack(pady=10, fill='x')
         tk.Button(btn_frame, text="Edit/Delete Quiz", font=arial_round, bg='#d19711', activebackground='#0ee832', relief="sunken", bd=5, padx=7, pady=3, command=self.edit_del_quiz).pack(pady=10, fill='x')
         tk.Button(btn_frame, text="Track Progress", font=arial_round, bg='#d19711', activebackground='#0ee832', relief="sunken", bd=5, padx=7, pady=3, command=self.track_progress).pack(pady=10, fill='x')
-        tk.Button(btn_frame, text="Back to Main Menu", font=arial_round, bg='#fc0000', activebackground='#0ee832', relief='raised', bd=5, padx=7, pady=3, command=lambda: self.root.destroy()).pack(pady=10, fill='x')
+        tk.Button(btn_frame, text="Back to Main Menu", font=arial_round, bg='#fc0000', activebackground='#0ee832', relief='raised', bd=5, padx=7, pady=3, command=lambda : self.root.destroy()).pack(pady=10, fill='x')
 
     def form_quiz(self, quiz_idx = None, quiz_data = None):
         #function to form a quiz form (for create quiz and edit quiz)
@@ -154,7 +154,7 @@ class flashcardQuizzer:
                     mb.showinfo("Success", "Quiz deleted successfully!")
                     self.edit_del_quiz()
             except IndexError:
-                mb.showwarning("No Selection!", "Please select a quiz to edit.")
+                mb.showwarning("No Selection!", "Please select a quiz to delete.")
             
         btn_frame = tk.Frame(self.root)
         btn_frame.pack(pady=20)
@@ -214,6 +214,8 @@ class flashcardQuizzer:
                         show_next_quiz()
                     else:
                         mb.showerror("Incorrect", "The selected answers is wrong. \nTry again!")
+                        for i in range(len(option_vars)):
+                            option_vars[i].set(0) #deselect wrong answer
                         self.save_progress_file()
 
                 def show_answer():
